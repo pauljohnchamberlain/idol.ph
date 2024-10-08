c56f580ad0122110f5e673d0d4b342ed405ec476
Paul John Chamberlain <114663999+pauljohnchamberlain@users.noreply.github.com>
2024-10-08 16:21:45 +0800

    added forgot component

---
+++ app/forgot/page.tsx
@@ -0,0 +1,12 @@
+// app/forgot/page.tsx
+
+import React from 'react';
+
+export default function ForgotPasswordPage() {
+	return (
+		<div>
+			<h1>Forgot Password</h1>
+			{/* Your form or content */}
+		</div>
+	);
+}
